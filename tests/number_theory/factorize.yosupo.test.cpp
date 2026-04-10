551ecac7e23dc12731bf65e6f1e755323bf5e299
sqh926 <itz.sqh@gmail.com>
2026-04-10 20:43:46 +0300

    Fix pollards-rho cpp file

--- tests/number_theory/factorize.yosupo.test.cpp
+++ tests/number_theory/factorize.yosupo.test.cpp
@@ -11,15 +11,18 @@ using namespace sqalg;
 i32 main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    u64 n; cin >> n;
-    auto factors = factorize(n);
-    i32 size = 0;
-    for (int i = 0; i < factors.size(); i++) size += factors[i].second;
-    cout << size << "\n";
-    for (const auto& [p, e] : factors) {
-        for(int i = 0; i < e; i++) {
-            cout << p << " ";
-        }
+    i32 q; cin >> q;
+    while (q-->0) {
+	u64 n; cin >> n;
+	auto factors = factorize(n);
+	i32 size = 0;
+	for (int i = 0; i < factors.size(); i++) size += factors[i].second;
+	cout << size << "\n";
+	for (const auto& [p, e] : factors) {
+	    for(int i = 0; i < e; i++) {
+		cout << p << " ";
+	    }
+	}
+	cout << "\n";
     }
-    cout << "\n";
 }
